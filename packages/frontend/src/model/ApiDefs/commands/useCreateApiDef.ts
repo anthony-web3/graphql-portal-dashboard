cce3ca507c45282c3ac591e5988c5fdafe41f0ef
Arthur Murauskas <arthur.murauskas@gmail.com>
2021-02-04 12:17:16 +0100

    Initial commit

---
+++ packages/frontend/src/model/ApiDefs/commands/useCreateApiDef.ts
@@ -0,0 +1,19 @@
+import { gql, useMutation } from '@apollo/client';
+
+const CREATE_API_DEF = gql`
+  mutation CreateApiDef($apiDef: CreateApiDef!, $sources: [ID!]!) {
+    createApiDef(apiDef: $apiDef, sources: $sources) {
+      _id
+      name
+    }
+  }
+`;
+
+export const useCreateApiDef = (options?: any) => {
+  const [createApiDef, { error, loading }] = useMutation(
+    CREATE_API_DEF,
+    options || {}
+  );
+
+  return { createApiDef, error, loading };
+};
--- packages/frontend/src/view/MyAPI/Create/Create.tsx
+++ packages/frontend/src/view/MyAPI/Create/Create.tsx
@@ -1,5 +1,69 @@
 import React from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { Helmet } from 'react-helmet';
+import {
+  HugeWidget,
+  Input,
+  PrimaryButton,
+  WidgetBody,
+  WidgetHeader,
+  WidgetRow,
+} from '../../../ui';
+import { useFormErrors } from '../../../model/Hooks';
+import { useCreateApiDef } from '../../../model/ApiDefs/commands';
 
-import { UnderConstruction } from '../../UnderConstruction';
+export const CreateApi: React.FC = () => {
+  const { handleSubmit, control, errors } = useForm();
+  const { createApiDef } = useCreateApiDef();
+  const onSubmit = (data: any) => {
+    console.log(data);
+    createApiDef({
+      variables: {
+        apiDef: {
+          name: data.name,
+          endpoint: data.endpoint,
+        },
+        sources: [],
+      },
+    });
+  };
 
-export const CreateApi:React.FC = () => <UnderConstruction name="Create API"/>
+  useFormErrors(errors);
+
+  return (
+    <>
+      <Helmet>
+        <title>Create a new GraphQL API</title>
+      </Helmet>
+      <WidgetRow>
+        <HugeWidget>
+          <WidgetHeader title="Create a new GraphQL API">
+            <PrimaryButton disabled={true}>Create API</PrimaryButton>
+          </WidgetHeader>
+          <WidgetBody>
+            <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+              <Controller
+                as={Input}
+                label="GraphQL API Name"
+                control={control}
+                required
+                name="name"
+                fullWidth
+              />
+              <Controller
+                as={Input}
+                control={control}
+                label="API endpoint"
+                name="endpoint"
+                required
+                helperText="Relative API path, for example: /graphql"
+                fullWidth
+              />
+              <PrimaryButton type="submit">Create API</PrimaryButton>
+            </form>
+          </WidgetBody>
+        </HugeWidget>
+      </WidgetRow>
+    </>
+  );
+};
